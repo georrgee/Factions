dcad06360e5de4a6193bbc2afceda2503e1d6b6c
George Garcia <garcia.george.b@gmail.com>
2019-04-17 20:58:51 -0700

    Initial Commit

---
+++ Factions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Factions
+//
+//  Created by George Garcia on 4/17/19.
+//  Copyright © 2019 GeeTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
